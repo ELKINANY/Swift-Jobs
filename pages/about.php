619495837ecd217282023eb016777d43a78a618a
mohamed <elkinanyformet@gmail.com>
2025-01-06 15:09:26 +0200

    Design

---
+++ pages/about.php
@@ -0,0 +1,32 @@
+<?php include "./head.php"?>
+    <!-- Navbar -->
+<?php include "./navBar.php" ?>
+
+    <!-- Main Content -->
+    <main class="container py-5">
+        <h1 class="text-center mb-5">من نحن</h1>
+        <div class="row">
+            <div class="col-md-3 mb-4">
+                <img src="../logo.jpg" alt="صورة عن الشركة">
+            </div>
+            <div class="col-md-9">
+                <h2 class="mb-3">مهمتنا</h2>
+                <p>نحن في "Swift Jobs" نسعى جاهدين لربط أصحاب العمل بالمواهب المناسبة. هدفنا هو تسهيل عملية التوظيف وجعلها أكثر كفاءة لكل من الشركات والباحثين عن عمل.</p>
+                <h2 class="mb-3 mt-4">رؤيتنا</h2>
+                <p>نطمح أن نكون المنصة الرائدة في مجال التوظيف في المنطقة، ونساهم في تقليل معدلات البطالة وتعزيز النمو الاقتصادي من خلال توفير فرص عمل مناسبة للجميع.</p>
+                <h2 class="mb-3 mt-4">قيمنا</h2>
+                <ul>
+                    <li>الشفافية</li>
+                    <li>الابتكار</li>
+                    <li>التميز في خدمة العملاء</li>
+                    <li>المسؤولية الاجتماعية</li>
+                </ul>
+            </div>
+        </div>
+    </main>
+
+    <!-- Footer -->
+    <?php include 'footer.php'; ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ pages/navBar.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg bg-white py-1">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="./index.php">
+                <img src="../logo.jpg" alt="وظفني" height="100" >
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto mb-2">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="./index.php">الرئيسية</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./about.php">من نحن</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./faq.php">الأسئلة الشائعة</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./contact.php">اتصل بنا</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">تسجيل الدخول</a>
+                    </li>
+                </ul>
+                <div class="d-flex gap-2">
+                    <button class="btn btn-custom-primary">سجل كفرد</button>
+                    <button class="btn btn-custom-primary">سجل كمنشأة</button>
+                </div>
+            </div>
+        </div>
+    </nav>
